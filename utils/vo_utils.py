cfbad5fd2dee187c08b4d1c41326c40dc456ac3e
Sergio <sergio.deleon@cimat.mx>
2025-01-19 17:20:37 -0600

    add camera class object

---
+++ utils/vo_utils.py
@@ -0,0 +1,78 @@
+import torch 
+DEVICE="cpu"
+
+def getProjectionMatrix2(znear, zfar, cx, cy, fx, fy, W, H):
+    left = ((2 * cx - W) / W - 1.0) * W / 2.0
+    right = ((2 * cx - W) / W + 1.0) * W / 2.0
+    top = ((2 * cy - H) / H + 1.0) * H / 2.0
+    bottom = ((2 * cy - H) / H - 1.0) * H / 2.0
+    left = znear / fx * left
+    right = znear / fx * right
+    top = znear / fy * top
+    bottom = znear / fy * bottom
+    P = torch.zeros(4, 4)
+
+    z_sign = 1.0
+
+    P[0, 0] = 2.0 * znear / (right - left)
+    P[1, 1] = 2.0 * znear / (top - bottom)
+    P[0, 2] = (right + left) / (right - left)
+    P[1, 2] = (top + bottom) / (top - bottom)
+    P[3, 2] = z_sign
+    P[2, 2] = z_sign * zfar / (zfar - znear)
+    P[2, 3] = -(zfar * znear) / (zfar - znear)
+
+    return P
+
+def getWorld2View2(R, t, translate=torch.tensor([0.0, 0.0, 0.0]), scale=1.0):
+    translate = translate.to(R.device)
+    Rt = torch.zeros((4, 4), device=R.device)
+    # Rt[:3, :3] = R.transpose()
+    Rt[:3, :3] = R
+    Rt[:3, 3] = t
+    Rt[3, 3] = 1.0
+
+    C2W = torch.linalg.inv(Rt)
+    cam_center = C2W[:3, 3]
+    cam_center = (cam_center + translate) * scale
+    C2W[:3, 3] = cam_center
+    Rt = torch.linalg.inv(C2W)
+    return Rt
+
+def image_gradient(image):
+    if image.dim() == 2:
+        image = image.unsqueeze(0)  # Add channel dimension
+
+    # Compute image gradient using Scharr Filter
+    c = image.shape[0]
+    conv_y = torch.tensor(
+        [[3, 0, -3], [10, 0, -10], [3, 0, -3]], dtype=torch.float32, device=DEVICE
+    )
+    conv_x = torch.tensor(
+        [[3, 10, 3], [0, 0, 0], [-3, -10, -3]], dtype=torch.float32, device=DEVICE
+    )
+    normalizer = 1.0 / torch.abs(conv_y).sum()
+    p_img = torch.nn.functional.pad(image, (1, 1, 1, 1), mode="reflect")[None]
+    img_grad_v = normalizer * torch.nn.functional.conv2d(
+        p_img, conv_x.view(1, 1, 3, 3).repeat(c, 1, 1, 1), groups=c
+    )
+    img_grad_h = normalizer * torch.nn.functional.conv2d(
+        p_img, conv_y.view(1, 1, 3, 3).repeat(c, 1, 1, 1), groups=c
+    )
+    return img_grad_v[0], img_grad_h[0]
+
+def image_gradient_mask(image, eps=0.01):
+    # Compute image gradient mask
+    c = image.shape[0]
+    conv_y = torch.ones((1, 1, 3, 3), dtype=torch.float32, device=DEVICE)
+    conv_x = torch.ones((1, 1, 3, 3), dtype=torch.float32, device=DEVICE)
+    p_img = torch.nn.functional.pad(image, (1, 1, 1, 1), mode="reflect")[None]
+    p_img = torch.abs(p_img) > eps
+    img_grad_v = torch.nn.functional.conv2d(
+        p_img.float(), conv_x.repeat(c, 1, 1, 1), groups=c
+    )
+    img_grad_h = torch.nn.functional.conv2d(
+        p_img.float(), conv_y.repeat(c, 1, 1, 1), groups=c
+    )
+
+    return img_grad_v[0] == torch.sum(conv_x), img_grad_h[0] == torch.sum(conv_y)
