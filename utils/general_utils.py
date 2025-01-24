3b6877c6915d9b1fce0af4123b2019f111b11e4f
Sergio <sergio.deleon@cimat.mx>
2025-01-24 14:54:19 -0600

    utils/general_utils.py

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -1,4 +1,6 @@
 import yaml
+import torch 
+from submodules.MiDaS.midas.model_loader import default_models, load_model
 
 def load_config(path):
     """
@@ -9,3 +11,15 @@ def load_config(path):
         cfg = yaml.full_load(f)
 
     return cfg 
+
+def init_model(model_type="dpt_swin2_tiny_256"):
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    optimize=False
+    side=False 
+    height=None 
+    square=False 
+    grayscale=False 
+    model_type=model_type
+    model_path="submodules/MiDaS/weights/dpt_swin2_tiny_256.pt" 
+    model, transform, net_w, net_h = load_model(device=device, model_path=model_path, model_type=model_type, optimize=optimize, height=height, square=square)
+    return model
